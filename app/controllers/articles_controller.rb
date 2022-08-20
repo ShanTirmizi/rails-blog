7f775eb46ebddbbe02aad89f81e2edf4020ec16c
Shan Tirmizi <tirmizishahnawaz@gmail.com>
2022-08-20 20:52:02 +0100

    Refactored the code to make it more reuseble

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,18 +1,19 @@
 # create a articles controller 
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   def index 
     @articles = Article.all
   end
+
   def show
-    @article = Article.find(params[:id])
   end 
 
   def new
     @article = Article.new
   end
 
- def create
+  def create
     @article = Article.new(article_params)
     if @article.save
       flash[:notice] = "Article was successfully created"
@@ -21,13 +22,11 @@ class ArticlesController < ApplicationController
       render 'new'
     end
   end
-
+ 
   def edit
-    @article = Article.find(params[:id])
   end
   
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice] = "Article was successfully updated"
       redirect_to @article
@@ -37,12 +36,11 @@ class ArticlesController < ApplicationController
   end
 
   # def destroy
-  #   @article = Article.find(params[:id])
+  #
   #   @article.destroy
   #   redirect_to articles_path
   # end
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
 
     respond_to do |format|
@@ -51,10 +49,12 @@ class ArticlesController < ApplicationController
     end
   end
 
-
   private
     def article_params
       params.require(:article).permit(:title, :description)
     end
-
+    
+    def set_article
+      @article = Article.find(params[:id])
+    end
 end 
