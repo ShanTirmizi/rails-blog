d316ad71be78c816f60e2a62e87b19b45a934854
Shan Tirmizi <tirmizishahnawaz@gmail.com>
2022-08-20 20:23:03 +0100

    Added the ability to delte articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,6 +21,7 @@ class ArticlesController < ApplicationController
       render 'new'
     end
   end
+
   def edit
     @article = Article.find(params[:id])
   end
@@ -33,6 +34,12 @@ class ArticlesController < ApplicationController
     else
       render 'edit'
     end
+  end
+  
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
   end
 
 
